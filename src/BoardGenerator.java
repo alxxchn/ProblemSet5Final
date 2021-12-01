54cdbdbe0d6bba23f4c8a0c178edb54a2b68bf58
megmessinger <messml20@wfu.edu>
2021-12-01 17:27:29 -0500

    Adding more to base code to create the background

--- src/BoardGenerator.java
+++ src/BoardGenerator.java
@@ -1,2 +1,17 @@
+import java.awt.*;
+import java.util.Scanner;
+
 public class BoardGenerator {
+    public void paint(Graphics g) {
+
+        // borders
+        g.setColor(Color.black);
+        g.fillRect(0, 0, 1000, 800);
+
+        // background
+        g.setColor(Color.white);
+        g.fillRect(8, 8, 985, 750);
+
+
+    }
 }
--- src/MemoryGame.java
+++ src/MemoryGame.java
@@ -2,9 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.Random;
 
 public class MemoryGame extends JPanel implements MouseListener {
-    private int[] position = new int[16];
+    private int[] position = {0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7};
     private String[] filenames = {"dog1.jpg", "dog2.jpg", "dog3.jpg", "dog4.jpg", "dog5.jpg", "dog6.jpg", "dog7.jpg", "dog8.jpg"};
     private int box1, box2;
     private boolean isfirstClick = true;
@@ -12,14 +13,29 @@ public class MemoryGame extends JPanel implements MouseListener {
     private int height = 150;
 
 
-    public void paint(Graphics g) {
+
+    public static void swap(int[] position){
+        Random rand = new Random();
         for (int i = 0; i < 16; i++){
-            int x = width * (i % 4);
-            int y = height * (i / 4);
+            position[i] = rand.nextInt(8);
+            // need to figure out how to make this only two, he told us but I can't remember :/
+        }
+    }
+
+    public void paint(Graphics g) {
+        BoardGenerator board = new BoardGenerator();
+        board.paint((Graphics2D) g);
+        swap(position);
+        for (int i = 0; i < 16; i++){
+            int x = width * (i % 4) + 125;
+            int y = height * (i / 4) + 110;
 
             g.setColor(Color.BLUE);
-            g.drawRect(x, y, width, height);
-            g.drawString(filenames[position[i]], x, y);
+            g.drawRect(x, y, width - 50, height - 50);
+            g.setColor(Color.cyan);
+            g.fillRect(x, y, width - 50, height - 50); // not sure how to put the image in
+            g.setColor(Color.black);
+            g.drawString(filenames[position[i]], x + 50, y + 50);
         }
 
     }
@@ -34,12 +50,12 @@ public class MemoryGame extends JPanel implements MouseListener {
 
         if (isfirstClick){
             box1 = p;
-            //show box 1
+            //show box 1, trouble with this
             isfirstClick = false;
         }
         else {
             box2 = p;
-            // show box 2
+            // show box 2, trouble with this
             isfirstClick = true;
         }
 
