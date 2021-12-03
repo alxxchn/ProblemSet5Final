d67da4716dbd7aab945d899dd7135d67bb26641d
alexchen <chenai21@gmail.com>
2021-12-03 14:01:03 -0500

    New

--- src/BoardGenerator.java
+++ src/BoardGenerator.java
@@ -1,8 +1,13 @@
+/**
+ * @brief The BoardGenerator class creates the background of the Memory Game.
+ *
+ * @author: Meg Messinger and Alex Chen
+ */
 import java.awt.*;
-import java.util.Scanner;
 
 public class BoardGenerator {
     public void paint(Graphics g) {
+        // creates the background of the game
 
         // borders
         g.setColor(Color.black);
@@ -12,6 +17,12 @@ public class BoardGenerator {
         g.setColor(Color.white);
         g.fillRect(8, 8, 985, 750);
 
+        // draw name
+        g.setColor(Color.blue);
+        g.setFont(new Font("serif", Font.BOLD, 40));
+        g.drawString("Memory Game", 375, 75);
+
 
     }
+
 }
--- src/MemoryGame.java
+++ src/MemoryGame.java
@@ -1,31 +1,90 @@
+/**
+ * @brief The MemoryGame class is a class that creates many methods and variables that are integral to creating the
+ * memory game. It creates the images, the cards, checks if the cards have been clicked on, checks for matches, and
+ * much more.
+ *
+ * @author: Meg Messinger and Alex Chen
+ */
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.Random;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class MemoryGame extends JPanel implements MouseListener {
-    private int[] position = {0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7};
+public class MemoryGame extends JPanel implements ActionListener, MouseListener {
+    // creates local variables
+    private int[] position = {0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7}; // array of positions, makes sure there are two of each
     private String[] filenames = {"dog1.jpg", "dog2.jpg", "dog3.jpg", "dog4.jpg", "dog5.jpg", "dog6.jpg", "dog7.jpg", "dog8.jpg"};
-    private int box1, box2;
-    private boolean isfirstClick = true;
-    private int width = 200;
-    private int height = 150;
+    private int box1, box2; // two boxes will be clicked on
+    private boolean isfirstClick = true; // checks if it is the first or second click
+    private int width = 200; // set width
+    private int height = 150; // set height
+    private int numMatches = 0; // checks the amount of matches there are
+    private BufferedImage[] images; // array of images
+    private boolean[] show = new boolean[position.length]; // array that will tell if the images will be shown
+    private Timer delayTimer; // helps to stop code from running too fast
+    private boolean isDelayed = false;
+
+    public MemoryGame(){
+        addMouseListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        images = new BufferedImage[filenames.length]; // images array created
+        // calls images
+        for(int i = 0; i < filenames.length; i++){
+            // uses try and catch because otherwise the computer will think that this is not possible in every case
+            try {
+                images[i] = ImageIO.read(new File(filenames[i]));
+            }
+            catch (IOException ex){
+                System.out.println("Failed to load image");
+                System.exit(0);
+            }
+        }
+        // swaps the positions around in the array
+        swap(position);
+        // delay to make sure that both boxes are flipped back over slowly if they are not equal
+        delayTimer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                show[box1] = false;
+                show[box2] = false;
+                isDelayed = false;
+                System.out.println("Calling repaint() from timer...");
+                repaint();
+            }
+        });
+        // makes sure that not all of the values in the show array are set as false
+        delayTimer.setRepeats(false);
 
 
+    }
 
     public static void swap(int[] position){
-        Random rand = new Random();
-        for (int i = 0; i < 16; i++){
-            position[i] = rand.nextInt(8);
-            // need to figure out how to make this only two, he told us but I can't remember :/
+        // swaps the values in the position array
+        for (int i = 0; i < position.length - 1; i++){
+            int tmp = position[i];
+            // ensures that the new random value will be at a position in the position array after the current position(i)
+            int rand = (int)(Math.random() * (position.length - i - 1)) + i + 1;
+            // flips the value of position[i] with the value of position[rand]
+            position[i] = position[rand];
+            position[rand] = tmp;
+
         }
     }
 
     public void paint(Graphics g) {
+        System.out.println("paint() method called");
+        // draws the cards
         BoardGenerator board = new BoardGenerator();
-        board.paint((Graphics2D) g);
-        swap(position);
+        board.paint(g);
         for (int i = 0; i < 16; i++){
             int x = width * (i % 4) + 125;
             int y = height * (i / 4) + 110;
@@ -35,31 +94,83 @@ public class MemoryGame extends JPanel implements MouseListener {
             g.setColor(Color.cyan);
             g.fillRect(x, y, width - 50, height - 50); // not sure how to put the image in
             g.setColor(Color.black);
-            g.drawString(filenames[position[i]], x + 50, y + 50);
+            if (show[i] == true) {
+                g.drawImage(images[position[i]], x, y, width - 50, height - 50, this);
+            }
         }
-
     }
+
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        // not sure if it's registering the mouse clicks
-        int x = e.getX();
-        int y = e.getY();
-        int i = (int)(x / width);
-        int j = (int)(y / height);
+
+        if (isDelayed == true) {
+            return;
+        }
+
+        // registers the mouse clicks
+        Scanner scnr = new Scanner(System.in);
+        // gets the x and y value of the box that is clicked
+        int x = e.getX() - 125;
+        int y = e.getY() - 110;
+        // finds the row and column of the box clicked
+        int i = (int) (x / width);
+        int j = (int) (y / height);
+        // finds the position of box in the show array
         int p = i + j * 4;
 
-        if (isfirstClick){
+        // sets box1 to the position found and flips over the image
+        if (isfirstClick) {
             box1 = p;
-            //show box 1, trouble with this
-            isfirstClick = false;
+            show[box1] = true;
+            System.out.println("Calling repaint() from 1st click");
+            repaint();
+            isfirstClick = false; // now second click
         }
+        // sets box2 to the position found and flips over the image
         else {
             box2 = p;
-            // show box 2, trouble with this
-            isfirstClick = true;
+            show[box2] = true;
+            isfirstClick = true; // back to first click
+            System.out.println("Calling repaint from 2nd click");
+            repaint();
+            // checks if the images are the same / values in the position array
+            if (position[box1] == position[box2]) {
+//                show[box1] = true;
+//                show[box2] = true;
+                System.out.println("box1 = " + box1);
+                System.out.println("box2 = " + box2);
+                System.out.println(Arrays.toString(show));
+                numMatches++; // adds to the number of matches
+            }
+            // goes to the delayTimer method
+            else {
+                isDelayed = true;
+                delayTimer.start();
+            }
         }
-
+        // ends the game
+        if (numMatches == 8){
+            System.out.println("Calling repaint() from winning if");
+            show[box2] = false;
+            show[box2] = true;
+            repaint();
+            System.out.println("You win! Continue? (y/n)");
+            String answer = scnr.next();
+            // keeps game going
+            if (answer.equals("y")){
+                numMatches = 0;
+                swap(position);
+                for (int k = 0; k < position.length; k++){
+                    show[k] = false;
+                }
+                repaint();
+            }
+            // stops the game
+            else{
+                System.exit(0);
+            }
+        }
     }
 
     @Override
@@ -79,6 +190,11 @@ public class MemoryGame extends JPanel implements MouseListener {
 
     @Override
     public void mouseExited(MouseEvent e) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
